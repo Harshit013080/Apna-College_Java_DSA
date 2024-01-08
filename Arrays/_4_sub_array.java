458114e1f3f02f094e28226e15bdff372be24826
Harshit013080 <harshitkhatsuriyacomputer@gmail.com>
2024-01-08 21:27:13 +0530

    subarray prog added

---
+++ Arrays/_4_sub_array.java
@@ -0,0 +1,17 @@
+package Apna_College_Java_DSA.Arrays;
+
+public class _4_sub_array {
+    public static void main(String[] args) {
+        int []arr={2,4,6,8,10};
+        for (int i=0;i<arr.length;i++){
+            System.out.println("["+arr[i]+"] ");
+            for(int j=i+1; j<arr.length; j++){
+                for (int k=i; k<=j; k++){
+                    System.out.print("["+arr[k]+"] ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
