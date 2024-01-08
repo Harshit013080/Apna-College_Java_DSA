53e6b4d53de3123d82df7270f5bb104879e07699
Harshit013080 <harshitkhatsuriyacomputer@gmail.com>
2024-01-08 21:05:12 +0530

    3-Pairs of array program

---
+++ _3_Pairs_in_Arrays.java
@@ -0,0 +1,13 @@
+package Apna_College_Java_DSA;
+
+class Pairs_in_Arrays {
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
+        for(int i = 0; i< arr.length; i++){
+            for(int j = i+1; j<arr.length; j++){
+                System.out.print("("+arr[i]+","+arr[j]+") ");
+            }
+            System.out.println();
+        }
+    }
+}
